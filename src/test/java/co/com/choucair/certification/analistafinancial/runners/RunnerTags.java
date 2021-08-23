8dac1ef0bcb3e69594e52a9b63c458e1bb7f7cd4
earv94 <earv94@gmail.com>
2021-08-23 02:11:28 -0500

    second test - first task

--- src/test/java/co/com/choucair/certification/analistafinancial/runners/RunnerTags.java
+++ src/test/java/co/com/choucair/certification/analistafinancial/runners/RunnerTags.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features="src/test/resources/features/academyChoucair.feature",
         tags = "@stories",
-        glue = "co.com.choucair.certification.proyectobase.stepdefinitions",
+        glue = "co.com.choucair.certification.analistafinancial.stepdefinitions",
         snippets = SnippetType.CAMELCASE
 )
 
