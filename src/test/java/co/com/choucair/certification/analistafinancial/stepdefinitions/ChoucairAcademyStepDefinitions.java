9dd5c5c0bb34e4754e3b998c0ae1e22a28f0fc8d
earv94 <earv94@gmail.com>
2021-08-23 04:17:15 -0500

    Selection course - No test. No hay ingreso a la página de los cursos de bancolombia como lo indica el curso, se prosigue pero se debe alertar al tutor

--- src/test/java/co/com/choucair/certification/analistafinancial/stepdefinitions/ChoucairAcademyStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/analistafinancial/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -2,6 +2,7 @@ package co.com.choucair.certification.analistafinancial.stepdefinitions;
 
 import co.com.choucair.certification.analistafinancial.tasks.Login;
 import co.com.choucair.certification.analistafinancial.tasks.OpenUp;
+import co.com.choucair.certification.analistafinancial.tasks.Search;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -19,9 +20,9 @@ public class ChoucairAcademyStepDefinitions {
         OnStage.theActorCalled("Brandon").wasAbleTo(OpenUp.thePage(), (Login.onThePage()));
     }
 
-    @When("^he search for the course Recursos Automatizacion Bancolombia on the choucair academy platform$")
-    public void heSearchForTheCourseRecursosAutomatizacionBancolombiaOnTheChoucairAcademyPlatform() {
-
+    @When("^he search for the course (.*) on the choucair academy platform$")
+    public void heSearchForTheCourseRecursosAutomatizacionBancolombiaOnTheChoucairAcademyPlatform(String course) {
+        OnStage.theActorInTheSpotlight().attemptsTo(Search.the(course));
     }
 
     @Then("^he finds the course called resources Recursos Automatizacion Bancolombia$")
